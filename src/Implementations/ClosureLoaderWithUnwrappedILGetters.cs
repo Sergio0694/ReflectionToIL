577c772050e30bfd1a8ade444064e35ecd59dcb1
Sergio Pedri <sergio0694@live.com>
2019-09-20 23:51:16 +0200

    Added unwrapped IL code

---
+++ src/Implementations/ClosureLoaderWithUnwrappedILGetters.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ReflectionToIL.Models;
+
+namespace ReflectionToIL.Implementations
+{
+    public sealed class ClosureLoaderWithUnwrappedILGetters
+    {
+        private readonly IReadOnlyList<ClosureFieldWithUnwrappedGetter> Fields;
+
+        private readonly int ReferenceCount;
+
+        private readonly int ByteSize;
+
+        private ClosureLoaderWithUnwrappedILGetters(IReadOnlyList<ClosureFieldWithUnwrappedGetter> fields, int references, int bytes)
+        {
+            Fields = fields;
+            ReferenceCount = references;
+            ByteSize = bytes;
+        }
+
+        public static ClosureLoaderWithUnwrappedILGetters GetLoaderForDelegate(Delegate instance)
+        {
+            IEnumerable<ClosureFieldWithUnwrappedGetter> CollectFields(Type type, IReadOnlyList<FieldInfo> parents)
+            {
+                foreach (FieldInfo field in type.GetFields())
+                {
+                    if (field.Name.StartsWith("CS$<>"))
+                    {
+                        // Create a new list of parents including the current field
+                        IReadOnlyList<FieldInfo> updatedParents = parents.Append(field).ToList();
+
+                        foreach (var nested in CollectFields(field.FieldType, updatedParents))
+                        {
+                            yield return nested;
+                        }
+                    }
+                    else yield return new ClosureFieldWithUnwrappedGetter(field, parents);
+                }
+            }
+
+            // Explore the closure type, initially passing an empty array of parents
+            var fields = CollectFields(instance.Target.GetType(), Array.Empty<FieldInfo>()).ToArray();
+
+            // Preload the dynamic getters
+            foreach (var field in fields)
+            {
+                field.BuildDynamicGetter();
+            }
+
+            // Calculate how many bytes we need
+            int
+                referenceCount = 0,
+                byteSize = 0;
+            foreach (var field in fields)
+            {
+                if (field.Info.FieldType.IsValueType) byteSize += Marshal.SizeOf(field.Info.FieldType);
+                else referenceCount++;
+            }
+
+            return new ClosureLoaderWithUnwrappedILGetters(fields, referenceCount, byteSize);
+        }
+
+        public unsafe ClosureData GetData(Delegate instance)
+        {
+            object[] references = ArrayPool<object>.Shared.Rent(ReferenceCount);
+            byte[] bytes = ArrayPool<byte>.Shared.Rent(ByteSize);
+            int
+                referenceOffset = 0, // Offset into the references array
+                byteOffset = 0;      // Offset into the bytes array 
+
+            for (int i = 0; i < Fields.Count; i++)
+            {
+                // Load the current field
+                object value = Fields[i].Getter(instance.Target);
+
+                // We need to handle value types and objects differently
+                if (Fields[i].Info.FieldType.IsValueType)
+                {
+                    // Pin the boxed value and get the source and destination references
+                    GCHandle handle = GCHandle.Alloc(value, GCHandleType.Pinned);
+                    void* p = handle.AddrOfPinnedObject().ToPointer();
+                    ref byte source = ref Unsafe.AsRef<byte>(p);
+                    ref byte destination = ref Unsafe.Add(ref bytes[0], byteOffset);
+                    int size = Marshal.SizeOf(Fields[i].Info.FieldType);
+
+                    // Copy the raw data of the value type into our bytes buffer
+                    Unsafe.CopyBlock(ref destination, ref source, (uint)size);
+                    byteOffset += size;
+
+                    handle.Free(); // Do NOT forget to do this
+                }
+                else references[referenceOffset++] = value;
+            }
+
+            return new ClosureData(references, ReferenceCount, bytes, ByteSize);
+        }
+    }
+}
--- src/Models/ClosureFieldWithUnwrappedGetter.cs
+++ src/Models/ClosureFieldWithUnwrappedGetter.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 
 namespace ReflectionToIL.Models
 {
-    public sealed class ClosureFieldWithGetter
+    public sealed class ClosureFieldWithUnwrappedGetter
     {
         public FieldInfo Info { get; }
 
-        public IReadOnlyList<ClosureFieldWithGetter> Parents { get; }
+        public IReadOnlyList<FieldInfo> Parents { get; }
 
         public Func<object, object> Getter { get; private set; }
 
-        public ClosureFieldWithGetter(FieldInfo info, IReadOnlyList<ClosureFieldWithGetter> parents)
+        public ClosureFieldWithUnwrappedGetter(FieldInfo info, IReadOnlyList<FieldInfo> parents)
         {
             Info = info;
             Parents = parents;
@@ -22,17 +23,26 @@ namespace ReflectionToIL.Models
         public void BuildDynamicGetter()
         {
             // Create a new dynamic method
-            Type ownerType = Info.DeclaringType;
+            FieldInfo[] hierarchy = Parents.Append(Info).ToArray();
+            Type ownerType = hierarchy[0].DeclaringType;
             DynamicMethod method = new DynamicMethod(
                 $"Get{Info.Name}",
                 typeof(object),             // The return type
-                new[] { typeof(object) },  // A single object parameter
+                new[] { typeof(object) },   // A single object parameter
                 ownerType);                 // The type that will own the new method
 
-            // Build the IL method
+            // Load and cast the argument
             ILGenerator il = method.GetILGenerator();
             il.Emit(OpCodes.Ldarg_0);
             il.Emit(OpCodes.Castclass, ownerType);
+
+            // Unroll the depth traversal until the last parent
+            foreach (FieldInfo parent in Parents)
+            {
+                il.Emit(OpCodes.Ldfld, parent);
+            }
+
+            // Get and box the target field
             il.Emit(OpCodes.Ldfld, Info);
             il.Emit(OpCodes.Box, Info.FieldType);
             il.Emit(OpCodes.Ret);
@@ -42,3 +52,4 @@ namespace ReflectionToIL.Models
         }
     }
 }
+
