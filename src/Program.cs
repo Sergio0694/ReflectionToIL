34f2a7aec4399f0dcd8fc3fd3567a41fab73caaf
Sergio Pedri <sergio0694@live.com>
2019-09-21 18:27:28 +0200

    Added info about the benchmark run

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,12 @@ namespace ReflectionToIL
     {
         public static void Main()
         {
+            /* =================
+             * BenchmarkDotNet
+             * ================
+             * In order to run this benchmark, compile this project in Release mode,
+             * then go to its bin\Release\netcoreapp3.0 folder, open a cmd prompt
+             * and type "dotnet ReflectionToIL.dll */
             BenchmarkRunner.Run<GetDataFromClosureBenchmark>();
         }
     }
